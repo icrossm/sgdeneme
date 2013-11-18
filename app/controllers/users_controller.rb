478cdace190d1a4dcbaa2a2f9993dcad75fa5ba9
irem <miremarslan@gmail.com>
2013-11-18 16:23:55 +0200

    Error_message page initiliazed in the shared folder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,15 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user=User.new
-    render "new"
+    
+     @user=User.new(params[:user])
+    if  @user.save
+      #Handle a successful save.
+    else  
+     @title="Sign up"
+      render 'new'
   end
+end
   
   
 end
