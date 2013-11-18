00a7ad8ff3448696215b96413c23743166b8024a
irem <miremarslan@gmail.com>
2013-11-18 23:37:43 +0200

    Added a Session controller and Sign in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Sgdeneme::Application.routes.draw do
+  
   resources :users
-
+ resources :sessions,:only => [:new, :create, :destroy]
   root :to => "pages#home"
   
   get '/contact' , :to => 'pages#contact'
   get '/about' , :to => 'pages#about'
   get '/help' , :to =>'pages#help'
   get '/signup' , :to =>'users#new'
+  get '/signin' , :to =>'sessions#new'
+  get '/signout' , :to =>'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
